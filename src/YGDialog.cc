3f424b3cf6f53865694c38b6e11f9b53a958788b
Michael Meeks <mmeeks@novell.com>
2006-05-25 09:50:12 +0000

    fix snafu
    svn path=/trunk/gtk/; revision=38536

--- src/YGDialog.cc
+++ src/YGDialog.cc
@@ -90,6 +90,11 @@ void YGDialog::setSize( long newWidth, long newHeight )
 	fprintf (stderr, "%s:%s %ld, %ld (%d, %d) def %d, %d\n", G_STRLOC, G_STRFUNC,
 		 newWidth, newHeight, m_oldSize.width, m_oldSize.height, defw, defh);
 
+	// FIXME: ugly hack to nail idly growing-window issue
+	if (m_oldSize.width == newWidth &&
+		(m_oldSize.height == newHeight ||
+		m_oldSize.height == newHeight - 1)) // urgh...[!]
+		return;
 	m_oldSize.width = newWidth;
 	m_oldSize.height = newHeight;
 
