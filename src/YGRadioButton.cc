e543a119e6bd836bbd06a478e4d5b7cf967ad60e
Ricardo Cruz <rpmcruz@alunos.dcc.fc.up.pt>
2006-07-19 16:14:14 +0000

    YGRadioButton and YGCheckBox now support bold font option.
    svn path=/trunk/gtk/; revision=38653

--- src/YGRadioButton.cc
+++ src/YGRadioButton.cc
@@ -63,38 +63,49 @@ YGUI::createRadioButtonGroup (YWidget *parent, YWidgetOpt &opt)
 
 class YGRadioButton : public YRadioButton, public YGWidget
 {
+	bool m_isBold;
+
 public:
 	YGRadioButton (const YWidgetOpt &opt,
 		       YGWidget         *parent,
-		       YCPString         label,
+		       YCPString         label_str,
 		       YRadioButtonGroup *rb_group,
 		       bool               checked)
 		:  YRadioButton (opt, label, rb_group),
 		   YGWidget (this, parent, true, GTK_TYPE_RADIO_BUTTON, NULL)
 	{
 		IMPL
-		// TODO: support opt.boldFont.value()
-
-		gtk_button_set_use_underline (GTK_BUTTON (getWidget()), TRUE);
-		g_signal_connect (G_OBJECT (getWidget()), "toggled",
-		                  G_CALLBACK (toggled_cb), this);
+		m_isBold = opt.boldFont.value();
 
 		// set group
 		GSList *group = ((YGRadioButtonGroup *) rb_group)->group;
 		if (group)
 			gtk_radio_button_set_group (GTK_RADIO_BUTTON (getWidget()), group);
 
-		setLabel (label);
+		// set label
+		gtk_button_set_use_underline (GTK_BUTTON (getWidget()), TRUE);
+		setLabel (label_str);
 		if (checked)
 			setValue (YCPBoolean (true));
+
+		g_signal_connect (G_OBJECT (getWidget()), "toggled",
+		                  G_CALLBACK (toggled_cb), this);
 	}
 
 	// YRadioButton
-	virtual void setLabel (const YCPString &label)
+	virtual void setLabel (const YCPString &text)
 	{
+		// NOTE: we can't just set a PangoFontDescription at the initialization
+		// because each gtk_button_set_label() creates a new label
 		IMPL
-		string str = YGUtils::mapKBAccel(label->value_cstr());
+		string str = YGUtils::mapKBAccel(text->value_cstr());
+		if (m_isBold)
+			str = "<b>" + str + "</b>";
 		gtk_button_set_label (GTK_BUTTON (getWidget()), str.c_str());
+		YRadioButton::setLabel (text);
+
+		GtkWidget *label = gtk_bin_get_child (GTK_BIN (getWidget()));
+		gtk_label_set_use_markup (GTK_LABEL (label), TRUE);
 	}
 
 	virtual void setValue (const YCPBoolean &checked)
@@ -143,41 +154,39 @@ YGUI::createRadioButton (YWidget *parent, YWidgetOpt &opt,
 
 class YGCheckBox : public YCheckBox, public YGWidget
 {
+	bool m_isBold;
+
 public:
-	static void toggled_cb (GtkBox *box, YGCheckBox *pThis)
-	{
-		IMPL;
-		GtkToggleButton *button = GTK_TOGGLE_BUTTON (box);
-		if (gtk_toggle_button_get_inconsistent (button))
-		{
-			gtk_toggle_button_set_inconsistent (button, false);
-			pThis->setValue (YCPBoolean (true));
-		}
-
-		pThis->emitEvent (YEvent::ValueChanged);
-	}
-
 	YGCheckBox(const YWidgetOpt &opt,
 		   YGWidget         *parent,
-		   YCPString         label,
+		   YCPString         label_str,
 		   bool              checked)
-		:  YCheckBox (opt, label),
+		:  YCheckBox (opt, label_str),
 		   YGWidget (this, parent, true, GTK_TYPE_CHECK_BUTTON, NULL)
 	{
 		IMPL
-		// TODO: add support for opt.boldFont.value()
+		m_isBold = opt.boldFont.value();
+
 		gtk_button_set_use_underline (GTK_BUTTON (getWidget()), TRUE);
+		setLabel (label_str);
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (getWidget()), checked);
+
 		g_signal_connect (G_OBJECT (getWidget ()), "toggled",
 		                  G_CALLBACK (toggled_cb), this);
-		setLabel (label);
-		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (getWidget()), checked);
 	}
 
-	virtual void setLabel (const YCPString &label)
+	// YCheckButton
+	virtual void setLabel (const YCPString &text)
 	{
-		IMPL;
-		gtk_button_set_label (GTK_BUTTON (getWidget()),
-			YGUtils::mapKBAccel (label->value_cstr()).c_str());
+		IMPL
+		string str = YGUtils::mapKBAccel(text->value_cstr());
+		if (m_isBold)
+			str = "<b>" + str + "</b>";
+		gtk_button_set_label (GTK_BUTTON (getWidget()), str.c_str());
+		YCheckBox::setLabel (text);
+
+		GtkWidget *label = gtk_bin_get_child (GTK_BIN (getWidget()));
+		gtk_label_set_use_markup (GTK_LABEL (label), TRUE);
 	}
 
 	virtual void setValue (const YCPValue &val)
@@ -196,6 +205,7 @@ public:
 
 		g_signal_handlers_unblock_by_func (getWidget(), (gpointer) toggled_cb, this);
 	}
+
 	virtual YCPValue getValue()
 	{
 		IMPL;
@@ -206,6 +216,19 @@ public:
 			return YCPVoid();
 		else
 			return YCPBoolean (gtk_toggle_button_get_active (button));
+	}
+
+	static void toggled_cb (GtkBox *box, YGCheckBox *pThis)
+	{
+		IMPL
+		GtkToggleButton *button = GTK_TOGGLE_BUTTON (box);
+		if (gtk_toggle_button_get_inconsistent (button))
+		{
+			gtk_toggle_button_set_inconsistent (button, false);
+			pThis->setValue (YCPBoolean (true));
+		}
+
+		pThis->emitEvent (YEvent::ValueChanged);
 	}
 
 	// YWidget
