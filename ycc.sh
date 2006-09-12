8fa81a62ea11f2e6b1bd43ea406d0dfe382ce190
Ricardo Cruz <rpmcruz@alunos.dcc.fc.up.pt>
2006-09-12 23:51:55 +0000

    Improved ycc.sh a bit: * added argument to use our test set (x) * ask at the begining if we want to run modules with the Qt frontend as well.
    svn path=/trunk/gtk/; revision=38765

--- ycc.sh
+++ ycc.sh
@@ -1,22 +1,40 @@
 #!/bin/sh
+# Usage: Run this script with no argument to be presented
+# with Yast modules. 'z' argument for Yast-core shipped
+# examples. 'x' for our own set of tests.
 
+ZENITY_BINARY=/opt/gnome/bin/zenity
 Y2BASE_BINARY=/usr/lib/YaST2/bin/y2base
 
 # any argument switches to the examples
 DEFAULT_DIR=/usr/share/YaST2/clients
-if test "z$1" != "z"; then
-    DEFAULT_DIR=/usr/share/doc/packages/yast2-core/libyui/examples
+if [ $# -eq 1 ]; then
+    if [ "$1" == "z" ]; then
+        DEFAULT_DIR=/usr/share/doc/packages/yast2-core/libyui/examples
+    fi
+    if [ "$1" == "x" ]; then
+        DEFAULT_DIR=.
+        # go to the test directory
+        # (this doesn't affect user's cwd because this
+        #  is running in its own shell.)
+        cd "`dirname $0`/test"
+    fi
 fi
+
+RUN_QT=0
+zenity --question --text "Run also the modules with Qt?" && RUN_QT=1
 
 module_list=`ls $DEFAULT_DIR/*.ycp | sed 's/.*\///'`
 
 while true; do
-    module=`/opt/gnome/bin/zenity --title "Yast Control Center" \
+    module=`$ZENITY_BINARY --title "Yast Control Center" \
             --list --column "Available modules:" $module_list`;
     if [ "$module" ]; then
-#	$Y2BASE_BINARY "$DEFAULT_DIR/$module" qt &
-	$Y2BASE_BINARY "$DEFAULT_DIR/$module" gtk
+        if [ $RUN_QT -eq 1 ]; then
+            $Y2BASE_BINARY "$DEFAULT_DIR/$module" qt &
+        fi
+        $Y2BASE_BINARY "$DEFAULT_DIR/$module" gtk
     else
-	exit 0;
+        exit 0;
     fi
 done
