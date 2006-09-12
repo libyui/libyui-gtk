f7cc02a4cf7bf071585ca31bb3dec530df6602b0
Ricardo Cruz <rpmcruz@alunos.dcc.fc.up.pt>
2006-09-12 00:43:11 +0000

    Re-worked the interface. Looks a lot like the yast-qt control center now.
    svn path=/trunk/gtk/; revision=38760

--- ycc/ycc.cpp
+++ ycc/ycc.cpp
@@ -3,11 +3,12 @@
 #include <string>
 #include <set>
 #include <map>
+#include <list>
 #include <gtk/gtk.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Definitions */
-static GdkColor bg_color = { 0, 255 << 8, 255 << 8, 255 << 8 };
 #define YAST_GROUPS  "/usr/share/applications/YaST2/groups/"
 #define YAST_ENTRIES "/usr/share/applications/YaST2/"
 #define ICONS "/usr/share/YaST2/theme/SuSELinux/icons/32x32/apps/"
@@ -58,23 +59,16 @@ void erase_from (std::string &str, const std::string &chars)
 
 /* The widget with the icons. */
 class View {
-	GtkWidget *m_widget, *m_vbox;
+	GtkWidget *m_widget;
 	GtkAdjustment *adjustment;
 	std::map <std::string, GtkListStore*> m_stores;
+	std::list <std::string> sort_keys;
 
 public:
 	View()
 	{
-// FIXME: view port not working. I dunno how to deal with gtk_adjustment :-/
-		adjustment = GTK_ADJUSTMENT (gtk_adjustment_new (0.0, 0.0, 1.0, 1.0, 2.0, 1.0));
-		m_widget = gtk_viewport_new (NULL, adjustment);
-
-		GtkWidget *box = gtk_event_box_new();
-		gtk_widget_modify_bg (box, GTK_STATE_NORMAL, &bg_color);
-
-		m_vbox = gtk_vbox_new (FALSE, 0);
-		gtk_container_add (GTK_CONTAINER (box), m_vbox);
-		gtk_container_add (GTK_CONTAINER (m_widget), box);
+		m_widget = gtk_notebook_new();
+		gtk_notebook_set_tab_pos (GTK_NOTEBOOK (m_widget), GTK_POS_LEFT);
 	}
 
 	~View() {}
@@ -82,28 +76,61 @@ public:
 	GtkWidget *getWidget()
 	{ return m_widget; }
 
-	void addGroup (const gchar *name, const gchar *nick)
+	void addGroup (const gchar *name, const gchar *icon_path,
+	               const gchar *nick, const gchar *sort_key)
 	{
-		GtkWidget *hbox, *label, *separator;
-		hbox = gtk_hbox_new (FALSE, 0);
-		label = gtk_label_new (("<b>" + std::string (name) + "</b>").c_str());
-		gtk_label_set_use_markup (GTK_LABEL (label), TRUE);
-		separator = gtk_hseparator_new();
-		gtk_box_pack_start (GTK_BOX (hbox), label, FALSE, FALSE, 0);
-		gtk_box_pack_start (GTK_BOX (hbox), separator, TRUE, TRUE, 6);
-		gtk_box_pack_start (GTK_BOX (m_vbox), hbox, FALSE, FALSE, 0);
+		// calculate position
+		int pos;
+		{
+			std::list <std::string>::iterator it;
+			for (it = sort_keys.begin(), pos = 0; it != sort_keys.end(); it++, pos++)
+				if (strcmp (it->c_str(), sort_key) >= 0)
+					break;
+			sort_keys.insert (it, sort_key);
+		}
 
+		// label widget
+		GtkWidget *tab_label, *image, *label;
+
+		GdkPixbuf *icon = NULL;
+		if (icon_path) {
+			GError *error = 0;
+			std::string path = ICONS + std::string (icon_path) + ".png";
+			icon = gdk_pixbuf_new_from_file (path.c_str(), &error);
+			if (!icon)
+				g_warning ("Could not load icon: %s.\nReason: %s", icon_path, error->message);
+		}
+
+		tab_label = gtk_hbox_new (FALSE, 0);
+		label = gtk_label_new (name);
+		if (icon)
+			image = gtk_image_new_from_pixbuf (icon);
+
+		if (icon)
+			gtk_box_pack_start (GTK_BOX (tab_label), image, FALSE, FALSE, 0);
+		gtk_box_pack_start (GTK_BOX (tab_label), label, TRUE, TRUE, icon ? 6 : 0);
+
+		// page widget
 		GtkListStore *store = gtk_list_store_new (3, G_TYPE_STRING, GDK_TYPE_PIXBUF, G_TYPE_STRING);
 		m_stores [nick] = store;
 
 		GtkWidget *icons_view;
 		icons_view = gtk_icon_view_new_with_model (GTK_TREE_MODEL (store));
-		gtk_widget_modify_base (icons_view, GTK_STATE_NORMAL, &bg_color);
 		gtk_icon_view_set_text_column   (GTK_ICON_VIEW (icons_view), 0);
 		gtk_icon_view_set_pixbuf_column (GTK_ICON_VIEW (icons_view), 1);
-		gtk_box_pack_start (GTK_BOX (m_vbox), icons_view, TRUE, TRUE, 0);
 		g_signal_connect(G_OBJECT (icons_view), "item-activated",
 		                 G_CALLBACK (executeCommand), this);
+
+		GtkWidget *page;
+		page = gtk_scrolled_window_new (NULL, NULL);
+		gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (page),
+			GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+		gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (page), GTK_SHADOW_IN);
+		gtk_container_add (GTK_CONTAINER (page), icons_view);
+
+		// add those to the notebook
+		gtk_widget_show_all (tab_label);
+		gtk_notebook_insert_page_menu (GTK_NOTEBOOK (m_widget), page, tab_label, NULL, pos);
 	}
 
 	void addEntry (const gchar *group, const gchar *name,
@@ -127,8 +154,6 @@ public:
 		GtkTreeIter iter;
 		gtk_list_store_append (store, &iter);
 		gtk_list_store_set (store, &iter, 0, name, 1, icon, 2, execute, -1);
-
-gtk_adjustment_value_changed (adjustment);
 	}
 
 	static void executeCommand (GtkIconView *iconview, GtkTreePath *path, View *pThis)
@@ -170,7 +195,7 @@ int main(int argc, char* argv[])
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title (GTK_WINDOW (window), "Suse Control Center");
-	gtk_window_set_default_size (GTK_WINDOW (window), 400, 300);
+	gtk_window_set_default_size (GTK_WINDOW (window), 650, 400);
 	g_signal_connect(G_OBJECT (window), "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
 	View view;
@@ -188,11 +213,16 @@ int main(int argc, char* argv[])
 
 				gchar* name = g_key_file_get_locale_string (file, "Desktop Entry", "Name", 0, NULL);
 				gchar *nick = g_key_file_get_string (file, "Desktop Entry", "X-SuSE-YaST-Group", NULL);
+				gchar *icon = g_key_file_get_string (file, "Desktop Entry", "Icon", NULL);
+				gchar *sort_key = g_key_file_get_string (file, "Desktop Entry",
+				                                         "X-SuSE-YaST-SortKey", NULL);
 				if (name && nick)
-					view.addGroup (name, nick);
+					view.addGroup (name, icon, nick, sort_key);
 
-				if (name) g_free (name);
-				if (nick) g_free (nick);
+				if (name)     g_free (name);
+				if (nick)     g_free (nick);
+				if (icon)     g_free (icon);
+				if (sort_key) g_free (sort_key);
 			}
 			g_key_file_free (file);
 		}
