bf0fad811a566004d4539d8149c93a03aea1e686
Michael Meeks <mmeeks@novell.com>
2007-02-06 16:48:12 +0000

    2007-02-06  Michael Meeks  <michael.meeks@novell.com>
            * configure.in.in, Makefile.cvs, VERSION, SUBDIRS,
            MAINTAINER, RPMNAME: move to a more yast-like build system.
            * configure.in: remove
            * autogen.sh: update - make -f Makefile.cvs first.
    
    
    svn path=/trunk/gtk/; revision=38836

--- autogen.sh
+++ autogen.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
 
-aclocal $ACLOCAL_FLAGS || exit 1;
-libtoolize --force --copy
-autoheader
-automake --gnu --add-missing --copy || exit 1;
-autoconf || exit 1;
-
-libdir=`pkg-config --print-errors --variable=libdir yast2-core`;
-
-if test "z$PKG_CONFIG_PATH" = "z"; then
-    export PKG_CONFIG_PATH="/opt/gnome/lib64/pkgconfig:/usr/lib64/pkgconfig;/opt/gnome/lib/pkgconfig:/usr/lib/pkgconfig"
-fi
-
-if test "z$@" = "z"; then
-    ./configure --prefix=/usr --libdir=$libdir
-
-elif test "x$NOCONFIGURE" = "x"; then
-    ./configure "$@"
-
-else
-    echo "Skipping configure process."
-fi
+echo
+echo "--------------------------------------------------------"
+echo "Warning: please use '$ make -f Makefile.cvs' instead ..."
+echo "--------------------------------------------------------"
+echo
+make -f Makefile.cvs
