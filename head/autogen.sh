5e36ad039545f4d09b8b47cc448c6fe5b9dc675f
Ricardo Cruz <rpmcruz@alunos.dcc.fc.up.pt>
2007-11-29 03:53:07 +0000

    svn path=/trunk/gtk/; revision=42489

---
+++ head/autogen.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo
+echo "--------------------------------------------------------"
+echo "Warning: please use '$ make -f Makefile.cvs' instead ..."
+echo "--------------------------------------------------------"
+echo
+make -f Makefile.cvs
