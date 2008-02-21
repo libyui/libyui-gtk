03f82ca06ecd1cd63ab047c9dbaff540d69c8621
Stephan Kulow <coolo@suse.de>
2008-02-21 06:25:58 +0000

    make gtk frontend work again
    svn path=/trunk/gtk/; revision=44717

--- src/Y2CCGtk.cc
+++ src/Y2CCGtk.cc
@@ -5,48 +5,17 @@
 #include <config.h>
 #include <YGUI.h>
 #include <ycp/y2log.h>
-#include <ycp-ui/Y2CCUI.h>
 #include <ycp-ui/YUIComponent.h>
 
-class YGUIComponent : public YUIComponent
+YUI * createUI( bool withThreads )
 {
-public:
+    static YGUI *_ui = 0;
+    
+    if ( ! _ui )
+    {
+        _ui = new YGUI( withThreads );
+    }
 
-	YGUIComponent() : YUIComponent() {}
-
-	virtual string name() const { return "gtk"; }
-
-	virtual YUI *createUI (bool with_threads)
-	{
-#ifdef IMPL_DEBUG
-		fprintf (stderr, "Create a gtk+ UI (with threads: %d) !\n", with_threads);
-#endif
-		return new YGUI (with_threads);
-	}
-};
-
-class Y2CCGtk : public Y2CCUI
-{
-public:
-	Y2CCGtk() : Y2CCUI() { };
-
-	bool isServerCreator () const { return true; };
-	
-	Y2Component *create (const char *name) const
-	{
-		y2milestone( "Creating %s component", name );
-		if (!strcmp (name, "gtk") ) {
-			Y2Component* ret = YUIComponent::uiComponent ();
-			if (!ret || ret->name () != name) {
-				y2debug ("UI component is %s, creating %s", ret? ret->name().c_str() : "NULL", name);
-				ret = new YGUIComponent();
-			}
-			return ret;
-		}
-		return 0;
-	}
-};
-
-// Singleton plugin registration instance.
-Y2CCGtk g_y2ccgtk;
+    return _ui;
+}
 
