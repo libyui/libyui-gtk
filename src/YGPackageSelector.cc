638c04ebb1c9ae31a7477d81df831a2845fa183e
Ricardo Cruz <rpmcruz@alunos.dcc.fc.up.pt>
2008-08-30 17:48:44 +0000

    Revert selector border.
    svn path=/trunk/gtk/; revision=50505

--- src/YGPackageSelector.cc
+++ src/YGPackageSelector.cc
@@ -2432,6 +2432,7 @@ public:
 		: YPackageSelector (NULL, mode),
 		  YGWidget (this, parent, true, YGTK_TYPE_WIZARD, NULL)
 	{
+		setBorder (0);
 		GtkWindow *window = YGDialog::currentWindow();
 		int enlarge_width = MIN (0.95 * YUI::app()->displayWidth(), 650);
 		int enlarge_height = MIN (0.95 * YUI::app()->displayHeight(), 600);
