626d6cc600c10c737862e7f29458dcbd61a870d6
Ricardo Cruz <rpmcruz@alunos.dcc.fc.up.pt>
2009-10-15 23:26:28 +0000

    ... continued.
    svn path=/trunk/gtk/; revision=59063

---
+++ src/Y2CCGtk.cc
@@ -0,0 +1,21 @@
+/********************************************************************
+ *           YaST2-GTK - http://en.opensuse.org/YaST2-GTK           *
+ ********************************************************************/
+
+#define YUILogComponent "gtk"
+#include <config.h>
+#include <YGUI.h>
+#include <YGi18n.h>
+
+YUI * createUI( bool withThreads )
+{
+    static YGUI *_ui = 0;
+
+    if ( ! _ui )
+    {
+        _ui = new YGUI( withThreads );
+    }
+
+    return _ui;
+}
+
---
+++ src/dummy.cc
@@ -0,0 +1,13 @@
+/********************************************************************
+ *           YaST2-GTK - http://en.opensuse.org/YaST2-GTK           *
+ ********************************************************************/
+
+#define YUILogComponent "gtk"
+#include <config.h>
+#include <YGUI.h>
+int main (int argc, char **argv)
+{
+	YGUI(false);
+	return 0;
+}
+
