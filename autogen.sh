87af961490d8059bf0dc28efa61906280251a0bd
Ricardo Cruz <rpmcruz@alunos.dcc.fc.up.pt>
2009-11-26 17:18:31 +0000

    * Reverted to development state.
    svn path=/trunk/gtk/; revision=59807

--- autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo
-echo "--------------------------------------------------------"
-echo "Warning: please use '$ make -f Makefile.cvs' instead ..."
-echo "--------------------------------------------------------"
-echo
-make -f Makefile.cvs
