9fc20ae691f731a1ea078530bcf80ad04581ea8a
Ricardo Cruz <rpmcruz@alunos.dcc.fc.up.pt>
2010-02-20 15:24:26 +0000

    * src/pkg/pkg-selector-help.h: updated help text.
    svn path=/trunk/gtk/; revision=60904

--- src/pkg/pkg-selector-help.h
+++ src/pkg/pkg-selector-help.h
@@ -5,68 +5,73 @@
 static const char *pkg_help[] = {
 	_("<h1>Purpose</h1>"
 	"<p>This tool lets you install, remove, and update applications.</p>"
-	"<p>&product; 's software management is also called 'package management'. A package is "
-	"generally an application bundle, but multiple packages that extend the application "
-	"may be offered in order to avoid clutter (e.g. games tend to de-couple the music "
-	"data in another package, since its not essential and requires significant disk space). "
-	"The base package will get the application's name, while extra packages are suffix-ed. "
-	"Common extras are:</p>"
+	"<p>Software in &product; is broken down and distributed in the form of "
+	"packages. This way, if multiple applications require a common system file, "
+	"this system file is shipped in its own package and is installed only once "
+	"if needed. The user need not be concerned about such underlying <i>dependencies</i>. "
+	"Likewise, the plugins and other non-essential data of a given application may "
+	"be shipped in their own packages, so the user may install them only if needed.</p>"),
+	_("<p>Common suffices for complementory packages:</p>"
 	"<ul>"
-	"<li>-plugin-: extends the application with some extra functionality.</li>"
-	"<li>-devel: needed for software development.</li>"
-	"<li>-debuginfo: needed for software beta-testing.</li>"
-	"<li>-fr, -dr, -pl (language siglas): translation files (your language package will "
-	"be marked for installation automatically).</li>"
+	"<li><b>-plugin-</b>: extends the application with extra functionality.</li>"
+	"<li><b>-devel</b>: headers for software development.</li>"
+	"<li><b>-debuginfo</b>: debug symbols for for software testing.</li>"
+	"<li><b>-fr</b>, <b>-pl</b> or other language siglas: translation files (your language "
+	"will be marked for installation automatically when needed).</li>"
 	"</ul>"),
-	_("<p>You will find both packages installed on your system, and packages that are made "
-	"available through the configured repositories.</p>"
-	"<blockquote>A repository is a packages media; it can either be local (such as the "
-	"&product; CDs), or a remote internet server. You can find utilities to setup "
-	"repositories on the YaST control center.</blockquote>"),
-	_("<h1>Usage</h1>"
-	"<h2>Install, Upgrade, Remove, Undo tab pages</h2>"
-	"<p>These tabs produce listings of the different sources of packages. 'Install' "
-	"lists available packages from the configured repositories. "
-	"'Upgrade' are the subset of installed packages that have more recent versions "
-	"available. 'Remove' allows you to uninstall a package that is installed in your "
-	"system. You can review changes in the 'Undo' tab, and revoke changes individually "
-	"at any time.</p>"
-	"<p>Press the 'Apply' button when you want your changes to be performed.</p>"),
-	_("<h2>Search</h2>"
+	_("<p>Both the packages that are installed on your system, and the packages "
+	"that are available by the <i>repositories</i> you have configured will be listed "
+	"together. <i>Status</i> filters are available in the right-bottom box.</p>"),
+	_("<blockquote>A repository is a packages media; it can either be local (such as the "
+	"installation CD), or a remote internet server. You can find an utility to setup "
+	"repositories on the YaST control center, which can also be accessed via the "
+	"<b>Configuration > Repositories</b> menu item.</blockquote>"),
+	_("<h1>Usage</h1>"),
+	_("<h2>Install, Upgrade, Remove, Undo tab pages</h2>"
+	"<p>All packages are listed together unless you have selected a <i>status</i> filter "
+	"from the right-bottom box. The check-box next to the package name indicates whether "
+	"the package is installed or not. If a more recent version of an installed "
+	"package is available, the version text will be highlighted in blue and an "
+	"upgrade button conveniently placed next to it. It is highlighted red if the "
+	"version installed is no longer being made available in any configured repository.</p>"
+	"<p>The context menu (right click on a package) provides extra options. "
+	"The <b>Undo</b> option can be used to revert any changes you have made. Multiple "
+	"packages may be selected (using the Control key) and modified together.</p>"
+	"<p>Use the <b>Version</b> list over the description box to select a specific version "
+	"of a package.</p>"
+	"<p>Press the <b>Apply</b> button when you want your changes to be performed.</p>"),
+	_("<h2>Lock software</h2>"
+	"<p>Packages can be locked against automatic changes via the context menu.</p>"
+	"<p>Locking is only useful in very unusual cases: for instance, you may not want "
+	"to install a given driver because it interferes with your system, yet you want "
+	"to install some collection that includes it. Locks can be applied whether the "
+	"package is installed or not.</p>"),
+	_("<h2>Filters</h2>"),
+	_("<h3>Search</h3>"
 	"<p>Enter free text into the search-field to match their names and descriptions. "
 	"(a search for 'office' will bring up the 'OpenOffice' packages as well as "
-	"AbiWord which carries the word 'office' in its description). You can also "
-	"choose to view software from a specific repository. Searching other attributes "
-	"is possible. For instance, you may perform a search on the installed package "
-	"containing a given file. Press the search icon for the filter menu.</p>"),
-	_("<h2>Groups, Patterns, Languages</h2>"
+	"'AbiWord' which carries the word 'office' in its description). You can search for "
+	"multiple keywords by separating the with a white space (e.g. 'spread sheet' "
+	"would return 'OpenOffice-calc').You may use the "
+	"search combined with a filter, like searching for a package in a given repository. "
+	"Other search attributes are provided, such as to search for a given file.</p>"),
+	_("<h3>Groups</h3>"
 	"<p>Software for &product; is indexed so that you can find software for a specific "
-	"task when you don't know the name of the software you are looking for. Browse "
-	"indexes of software through the Groups pane. A more detailed, hierarchical classification "
-	"of software packages, like 'Multimedia/Video' is available through the 'Detailed' check "
-	"button. 'Patterns' and 'Languages' are task-oriented collections of multiple packages that "
-	"install like one (the installation of the 'server'-pattern for example will install various "
-	"software needed for running a server). By using 'Install All' you make sure that future "
-	"collection changes, when you upgrade &product;, will be honored.</p>"),
+	"task when you are not aware of the software selection available. A more detailed, "
+	"hierarchical classification is provided by the <b>RPM Groups</b> filter.</p>"),
+	_("<h3>Patterns and Languages</h3>"
+	"<p><b>Patterns</b> are task-oriented collections of multiple packages that "
+	"install like one. The installation of the <i>File Server</i> pattern, for example, "
+	"will install various packages needed for running such a server.</p>"
+	"<p>If you want to install a particular language, you may want to do so via the "
+	"<b>Language</b> tool from the YaST control center.</p>"),
 	_("<h2>Software details in the box below</h2>"
 	"<p>Explore the available information about the package in the box below. Note "
-	"that more information is available for installed software than that only "
+	"that more information is available for installed packages than for those only "
 	"available from a repository.</p>"
 	"<p>You can also pick a specific version of the package to install from this "
-	"box.</p>"
-	"<p>The box will be placed to the right on large wide-screens when on fullscreen, "
-	"in order to better use the screen space.</p>"),
-	_("<h2>Lock software</h2>"
-	"<p>Advanced users may want to know that locking or unlocking a package against "
-	"automatic changes can be perfomed by right-clicking the package list entry.</p>"
-	"<p>Locking is only useful in very unusual cases: for instance, you may not want "
-	"to install a given drivers because it interferes with your system, yet you want "
-	"to install some collection that includes it.</p>"),
-	_("<h2>Apply button</h2>"
-	"<p>Changes will be performed once you decide to click the 'Apply' "
-	"button in the lower-right corner. If you want to leave the software manager "
-	"without performing any changes, simply press 'Cancel'.</p>"),
-	"<p>http://en.opensuse.org/YaST2-GTK</p>",
+	"box.</p>"),
+	"<hr/><p>http://en.opensuse.org/YaST2-GTK</p>",
 	0
 };
 
@@ -78,17 +83,18 @@ static const char *patch_help[] = {
 	"<h2>Categories</h2>"
 	"<p>Patches are grouped as follows:</p>"
 	"<ul>"
-	"<li>Security: patches a software flaw that could be exploited to gain "
+	"<li><b>Security</b>: patches a software flaw that could be exploited to gain "
 	"restricted privilege.</li>"
-	"<li>Recommended: fixes non-security related flaws (e.g. data corruption, "
+	"<li><b>Recommended</b>: fixes non-security related flaws (e.g. data corruption, "
 	"performance slowdown)</li>"
-	"<li>Optional: ones that only apply to few users.</li>"
+	"<li><b>Optional</b>: ones that only apply to few users.</li>"
+	"<li><b>Documentation</b>: fixes documentation errors.</li>"
+	"<li><b>YaST</b>: patches for the YaST control center tools.</li>"
 	"</ul>"),
-	_("<p>Only patches that apply to your system will be visible. &product; developers "
-	"are very restrained in pushing patches; you can be sure that all patches are "
-	"of signficant severity.</p>"
-	"<p>If you are looking for applications enhancements, you should check for upgrades "
-	"on the Software Manager.</p>"),
+	_("<p>Only patches that apply to your system will be visible. You can be sure "
+	"that the decision to make a patch available is not done trivially.</p>"
+	"<p>If you are looking for applications enhancements, you should check for <i>upgrades</i> "
+	"on the <b>Software Manager</b>.</p>"),
 	0
 };
 
