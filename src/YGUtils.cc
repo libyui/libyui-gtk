ffb5575ede0abc431dc1cee065a63ab98d7a4ea5
Ricardo Cruz <rpmcruz@alunos.dcc.fc.up.pt>
2010-02-24 16:51:59 +0000

    * src/YGUtils.cc: use remove icon instead of clear, 	and apply instead of quit (see bug 580848).
    svn path=/trunk/gtk/; revision=61007

--- src/YGUtils.cc
+++ src/YGUtils.cc
@@ -617,6 +617,10 @@ const char *YGUtils::mapStockIcon (const std::string &label)
 					_id = GTK_STOCK_GO_BACK;
 				else if (!strcmp (id, GTK_STOCK_MEDIA_RECORD))
 					_id = GTK_STOCK_SAVE;
+				else if (!strcmp (id, GTK_STOCK_CLEAR))
+					_id = GTK_STOCK_DELETE;
+				else if (!strcmp (id, GTK_STOCK_QUIT))
+					_id = GTK_STOCK_APPLY;
 				stockMap[cutUnderline (item.label)] = _id;
 			}
 			// some may not have a stock item because they can't be set on a label
