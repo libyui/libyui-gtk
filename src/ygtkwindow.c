d68b397f65aeb271962b545b404e61fe2395db90
Michael Meeks <mmeeks@novell.com>
2012-04-27 16:05:48 +0000

    hook a few methods to help out with size debugging.
    svn path=/trunk/gtk/; revision=68040

--- src/ygtkwindow.c
+++ src/ygtkwindow.c
@@ -18,18 +18,25 @@ static void ygtk_window_init (YGtkWindow *window)
 GtkWidget* ygtk_window_new (void)
 { return g_object_new (YGTK_TYPE_WINDOW, NULL); }
 
-void ygtk_window_get_preferred_width (GtkWidget *widget,
-	gint *minimum_width, gint *natural_width)
+static void ygtk_window_get_preferred_width (GtkWidget *widget,
+					     gint *minimum_width, gint *natural_width)
 {
 	GTK_WIDGET_CLASS (ygtk_window_parent_class)->get_preferred_width(widget, minimum_width, natural_width);
 	*minimum_width = 0;
 }
 
-void ygtk_window_get_preferred_height (GtkWidget *widget,
-	gint *minimum_height, gint *natural_height)
+static void ygtk_window_get_preferred_height (GtkWidget *widget,
+					      gint *minimum_height, gint *natural_height)
 {
 	GTK_WIDGET_CLASS (ygtk_window_parent_class)->get_preferred_height(widget, minimum_height, natural_height);
 	*minimum_height = 0;
+}
+
+static void
+ygtk_window_size_allocate (GtkWidget     *widget,
+			   GtkAllocation *allocation)
+{
+	GTK_WIDGET_CLASS (ygtk_window_parent_class)->size_allocate(widget, allocation);
 }
 
 static void ygtk_window_class_init (YGtkWindowClass *klass)
@@ -39,5 +46,6 @@ static void ygtk_window_class_init (YGtkWindowClass *klass)
 	GtkWidgetClass* widget_class = GTK_WIDGET_CLASS (klass);
 	widget_class->get_preferred_width = ygtk_window_get_preferred_width;
 	widget_class->get_preferred_height = ygtk_window_get_preferred_height;
+	widget_class->size_allocate = ygtk_window_size_allocate;
 }
 
