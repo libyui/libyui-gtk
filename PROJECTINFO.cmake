013c404330b998ab5e927c49eedaf232f219b3a8
Björn Esser <bjoern.esser@gmail.com>
2012-06-14 18:37:55 +0200

    update from buildtools   * much improves   * FindGTK3 automagick   * fix indention

--- PROJECTINFO.cmake
+++ PROJECTINFO.cmake
@@ -4,10 +4,10 @@ SET( BASELIB		"yui" )		# don't change this
 
 ##### MAKE ALL NEEDED CHANGES HERE #####
 
-SET( SUBDIRS		src  )	# set the subdirs where your code is located.
-SET( PLUGINNAME		"gtk" )		# define the your plugin's name, e.g. {gtk, ncurses-pkg, qt-graph}
-SET( LIB_DEPS		GTK )		# define the plugin's dependecies CMake should look for separated with spaces; libyui-internal deps go in the next line
-SET( INTERNAL_DEPS	)		# include libyui or it's plugins (if needed), too!
-SET( LIB_LINKER		)		# define the libs to link against with their -l name for separated with spaces, e.g. pthread dl; include libyui or it's plugins (if needed), too!
-SET( EXTRA_INCLUDES	)		# set include-dir which are not picked by CMake automagically here.
-SET( PROGSUBDIR		"" )		# define your programm's name if the plugin is made for it, only. e.g. "YaST2" for {gtk,ncurses,qt}-pkg.
+SET( SUBDIRS		src )
+SET( PLUGINNAME		"gtk" )
+SET( LIB_DEPS		GTK3 )
+SET( INTERNAL_DEPS	Libyui )
+SET( LIB_LINKER		)
+SET( EXTRA_INCLUDES	)
+SET( PROGSUBDIR		"" )
--- SOURCECONF.cmake
+++ SOURCECONF.cmake
@@ -1,75 +1,69 @@
 SET( ${TARGETLIB}_SOURCES
-dummy.cc
-test.cc
-Y2CCGtk.cc
-YGBarGraph.cc
-YGComboBox.cc
-YGDialog.cc
-ygdkmngloader.c
-YGDumbTab.cc
-YGFrame.cc
-YGImage.cc
-YGInputField.cc
-YGIntField.cc
-YGLabel.cc
-YGLayout.cc
-YGMenuButton.cc
-YGPackageSelectorPluginStub.cc
-YGProgressBar.cc
-YGPushButton.cc
-YGRadioButton.cc
-YGSelectionStore.cc
-YGText.cc
-ygtkbargraph.c
-ygtkfieldentry.c
-ygtkfixed.c
-ygtkhtmlwrap.c
-ygtkimage.c
-ygtklinklabel.c
-ygtkmenubutton.c
-ygtkratiobox.c
-ygtkrichtext.c
-ygtksteps.c
-ygtktextview.c
-ygtktimezonepicker.c
-ygtktreeview.c
-ygtkwindow.c
-ygtkwizard.c
-YGTreeView.cc
-YGUI.cc
-YGUtils.cc
-YGWidget.cc
-YGWizard.cc
+  dummy.cc
+  test.cc
+  Y2CCGtk.cc
+  YGBarGraph.cc
+  YGComboBox.cc
+  YGDialog.cc
+  ygdkmngloader.c
+  YGDumbTab.cc
+  YGFrame.cc
+  YGImage.cc
+  YGInputField.cc
+  YGIntField.cc
+  YGLabel.cc
+  YGLayout.cc
+  YGMenuButton.cc
+  YGPackageSelectorPluginStub.cc
+  YGProgressBar.cc
+  YGPushButton.cc
+  YGRadioButton.cc
+  YGSelectionStore.cc
+  YGText.cc
+  ygtkbargraph.c
+  ygtkfieldentry.c
+  ygtkfixed.c
+  ygtkhtmlwrap.c
+  ygtkimage.c
+  ygtklinklabel.c
+  ygtkmenubutton.c
+  ygtkratiobox.c
+  ygtkrichtext.c
+  ygtksteps.c
+  ygtktextview.c
+  ygtktimezonepicker.c
+  ygtktreeview.c
+  ygtkwindow.c
+  ygtkwizard.c
+  YGTreeView.cc
+  YGUI.cc
+  YGUtils.cc
+  YGWidget.cc
+  YGWizard.cc
 )
 
 SET( ${TARGETLIB}_HEADERS
-config.h
-YGDialog.h
-ygdkmngloader.h
-YGi18n.h
-YGPackageSelectorPluginIf.h
-YGSelectionStore.h
-ygtkbargraph.h
-ygtkfieldentry.h
-ygtkfixed.h
-ygtkhtmlwrap.h
-ygtkimage.h
-ygtklinklabel.h
-ygtkmenubutton.h
-ygtkratiobox.h
-ygtkrichtext.h
-ygtksteps.h
-ygtktextview.h
-ygtktimezonepicker.h
-ygtktreeview.h
-ygtkwindow.h
-ygtkwizard.h
-YGUI.h
-YGUtils.h
-YGWidget.h
+  YGDialog.h
+  ygdkmngloader.h
+  YGi18n.h
+  YGPackageSelectorPluginIf.h
+  YGSelectionStore.h
+  ygtkbargraph.h
+  ygtkfieldentry.h
+  ygtkfixed.h
+  ygtkhtmlwrap.h
+  ygtkimage.h
+  ygtklinklabel.h
+  ygtkmenubutton.h
+  ygtkratiobox.h
+  ygtkrichtext.h
+  ygtksteps.h
+  ygtktextview.h
+  ygtktimezonepicker.h
+  ygtktreeview.h
+  ygtkwindow.h
+  ygtkwizard.h
+  YGUI.h
+  YGUtils.h
+  YGWidget.h
 )
-
-SET( EXAMPLES_LIST
-  ##### Here go the examples
-)
-
