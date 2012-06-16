0aa751d42147f6f74caa2d07ef02d00e8f63e19c
Björn Esser <bjoern.esser@gmail.com>
2012-06-16 09:21:44 +0200

    update from buildtools

--- macros.cmake
+++ macros.cmake
@@ -33,7 +33,7 @@ ENDMACRO( SET_OPTIONS )
 MACRO( SET_BUILD_FLAGS )	# setup compiler-flags depending on CMAKE_BUILD_TYPE
 
   SET( CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined" )
-  
+
   STRING( STRIP "${CMAKE_CXX_FLAGS}" CMAKE_CXX_FLAGS )
   SET( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmessage-length=0" )
   STRING( STRIP "${CMAKE_CXX_FLAGS}" CMAKE_CXX_FLAGS )
@@ -128,7 +128,7 @@ MACRO( SET_ENVIRONMENT )	# setup the environment vars
   SET( FULL_DATA_DIR "${DATAROOTDIR}/lib${BASELIB}" )
   SET( INSTALL_INCLUDE_DIR "${INCLUDE_DIR}" CACHE PATH "Installation directory for header files" )
   SET( INSTALL_LIB_DIR "${LIB_DIR}" CACHE PATH "Installation directory for libraries" )
-  
+
   SET( THEME_DIR "${FULL_DATA_DIR}/theme" )
 
   IF( NOT DEFINED DOC_DIR )
@@ -155,8 +155,8 @@ MACRO( FIND_LIB_DEPENDENCIES )	# try to find all libs from ${LIB_DEPS}
 
   FOREACH( p ${LIB_DEPS} )
     FIND_PACKAGE( ${p} REQUIRED )
-    SET( CMAKE_INCLUDE_PATH "${${p}_INCLUDE_DIR}" "${CMAKE_INCLUDE_PATH}" )
-    SET( CMAKE_INCLUDE_PATH "${${p}_INCLUDE_DIRS}" "${CMAKE_INCLUDE_PATH}" )
+    SET( CMAKE_INCLUDE_PATH "${CMAKE_INCLUDE_PATH}" ${${p}_INCLUDE_DIR} )
+    SET( CMAKE_INCLUDE_PATH "${CMAKE_INCLUDE_PATH}" ${${p}_INCLUDE_DIRS} )
     SET( LIB_LINKER "${${p}_LIBRARIES}" "${LIB_LINKER}" )
   ENDFOREACH()
 
@@ -165,6 +165,10 @@ MACRO( FIND_LIB_DEPENDENCIES )	# try to find all libs from ${LIB_DEPS}
       SET( SONAME${p} "${Lib${BASELIB}_SONAME${p}}")
     ENDFOREACH()
   ENDIF( PLUGINNAME )
+
+  IF( QT_FOUND )
+    SET( CMAKE_INCLUDE_PATH "${CMAKE_INCLUDE_PATH}" ${QT_INCLUDES} )
+  ENDIF( QT_FOUND )
 
 ENDMACRO( FIND_LIB_DEPENDENCIES )
 
@@ -208,7 +212,7 @@ MACRO( SET_AUTODOCS )		# looks for doxygen, dot and latex and setup autodocs acc
     FIND_PACKAGE( LATEX )
 
     SET(
-      LATEX_COND 
+      LATEX_COND
       NOT ${PDFLATEX_COMPILER} STREQUAL "PDFLATEX_COMPILER-NOTFOUND"
       AND NOT ${MAKEINDEX_COMPILER} STREQUAL "MAKEINDEX_COMPILER-NOTFOUND"
     )
@@ -261,7 +265,7 @@ MACRO( SET_AUTODOCS )		# looks for doxygen, dot and latex and setup autodocs acc
       ${PROJECT_BINARY_DIR}/Doxyfile
       @ONLY
     )
-  
+
     CONFIGURE_FILE(
       ${PROJECT_BINARY_DIR}/Doxyfile
       ${PROJECT_BINARY_DIR}/Doxyfile
@@ -274,7 +278,7 @@ ENDMACRO( SET_AUTODOCS )
 
 MACRO( SET_SUBDIRS )		# setup the subdirs
 
-  FOREACH( p ${SUBDIRS} )  
+  FOREACH( p ${SUBDIRS} )
     ADD_SUBDIRECTORY( ${p} )
   ENDFOREACH()
 
@@ -293,7 +297,7 @@ MACRO( GEN_EXPORTS )		# generate and export the library-depends
       FILE "${PROJECT_BINARY_DIR}/${PROJECTNAME_UC}LibraryDepends.cmake"
     )
   ENDIF( ENABLE_STATIC AND NOT DISABLE_SHARED )
- 
+
   EXPORT( PACKAGE ${PROJECTNAME} )
 
 ENDMACRO( GEN_EXPORTS )
@@ -347,12 +351,12 @@ MACRO( PREP_SPEC_FILES )
   SET( SPEC_Owndir "%dir %{_libdir}${SPEC_Libdir}" )
   ENDIF( PLUGINNAME )
 
-  FOREACH( p "BuildRequires" "Conflicts" "Provides" "Obsoletes" "DEVEL_Requires" "DEVEL_Provides" )
+  FOREACH( p "BuildRequires" "Requires" "Provides" "Obsoletes" "Conflicts" "DEVEL_Requires" "DEVEL_Provides" )
     STRING( REPLACE "DEVEL_" "" SPEC_PREPEND "${p}" )
     FOREACH( x ${SPEC_${p}} )
       SET( ${p} "${${p}}${SPEC_PREPEND}:	${x}\n" )
     ENDFOREACH( x SPEC_${p} )
-  ENDFOREACH( p "BuildRequires" "Conflicts" "Provides" "Obsoletes" "DEVEL_Requires" "DEVEL_Provides" )
+  ENDFOREACH( p "BuildRequires" "Requires" "Provides" "Obsoletes" "Conflicts" "DEVEL_Requires" "DEVEL_Provides" )
 
   CONFIGURE_FILE(
     "${PROJECT_SOURCE_DIR}/resource/template.spec.in"
