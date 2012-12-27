deb3c484e12a22736c7fa2b07bd409abc1d85a3c
Angelo Naselli <anaselli@linux.it>
2012-12-27 21:43:09 +0100

    Added percentage on bar

--- src/YGProgressBar.cc
+++ src/YGProgressBar.cc
@@ -56,12 +56,13 @@ public:
 		GtkProgressBar *bar = GTK_PROGRESS_BAR (getWidget());
 		float fraction = CLAMP ((float) value / maxValue(), 0, 1);
 		gtk_progress_bar_set_fraction (bar, fraction);
-		gtk_main_iteration_do(false);
-/*
+
 		char *text = g_strdup_printf ("%d %%", (int) (fraction*100));
 		gtk_progress_bar_set_text (bar, text);
 		g_free (text);
-*/
+		gtk_progress_bar_set_show_text(bar, true);
+
+		gtk_main_iteration_do(false);
 	}
 
 	virtual unsigned int getMinSize (YUIDimension dim)
