69bb438fc7a12a9dbadceee87f87b1558e3afdb4
Angelo Naselli <anaselli@linux.it>
2021-04-15 22:27:10 +0200

    Managed so version from libyui

--- VERSION.cmake
+++ VERSION.cmake
@@ -5,7 +5,7 @@ SET( VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}${VERSION_SHA1}"
 
 ##### This is need for the libyui core, ONLY.
 ##### These will be overridden from exports in LibyuiConfig.cmake
-SET( SONAME_MAJOR "0" )
-SET( SONAME_MINOR "0" )
-SET( SONAME_PATCH "0" )
-SET( SONAME "${SONAME_MAJOR}.${SONAME_MINOR}.${SONAME_PATCH}" )
+#SET( SONAME_MAJOR "0" )
+#SET( SONAME_MINOR "0" )
+#SET( SONAME_PATCH "0" )
+#SET( SONAME "${SONAME_MAJOR}.${SONAME_MINOR}.${SONAME_PATCH}" )
